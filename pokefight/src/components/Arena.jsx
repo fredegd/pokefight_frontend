f4099dfa42816569c4e502ab8801cd100527262a
fredegd <124848643+fredegd@users.noreply.github.com>
2023-07-06 19:46:58 +0200

    selection Added

--- pokefight/src/components/Arena.jsx
+++ pokefight/src/components/Arena.jsx
@@ -4,37 +4,37 @@ import axios from "axios";
 
 export default function Arena({ playerName, selection }) {
   console.log(selection);
-  const [pokemonPlayer, setPokemonPlayer] = useState();
+  const [pokemonPlayer1, setPokemonPlayer1] = useState();
 
+  // fetch the pokemon for player 1
   useEffect(() => {
     selection &&
       axios
         .get(`https://pokeapi.co/api/v2/pokemon/${selection}`)
         .then((response) => {
           console.log(response.data);
-          setPokemonPlayer(response.data);
+          setPokemonPlayer1(response.data);
         })
         .catch((err) => {
           console.log(err);
         });
   }, [selection]);
-  console.log(pokemonPlayer);
+  console.log(pokemonPlayer1);
   //calculate a random number to assign a pokemn to the computer
 
-  // fetch the pokemon for player 1
   // fetch the pokemon for computer
 
-  //fight happens here
+  //let the  fight happens here
 
   //push the result to the database
 
   return (
     <div>
-     {pokemonPlayer &&
+     {pokemonPlayer1 &&
      <>
      <h1>Player Name: {playerName}</h1>
-     <p>fight with {pokemonPlayer.name}</p>
-     <img src={pokemonPlayer.sprites.back_default} alt={pokemonPlayer.name} />
+     <p>fight with {pokemonPlayer1.name}</p>
+     <img src={pokemonPlayer1.sprites.back_default} alt={pokemonPlayer1.name} />
      </> 
      
        }
