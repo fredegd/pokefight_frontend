c60943c6d9f818e9ee937dc018e6ad066f54d8a6
Alina Gattermann <gattermannalina@gmail.com>
2023-07-07 09:24:47 +0200

    battleground styling

--- pokefight/src/components/FightEnvironment.jsx
+++ pokefight/src/components/FightEnvironment.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { useState} from "react";
+import { useState } from "react";
+import '../components/battle.css';
 
 export default function FightEnvironment() {
 
 
-  
- 
+
+
   return (
-    <div>Fight</div>
+    <div className='battleground'>
+      <button className='fightbutton'>Fight</button>
+      <div className='player'></div>
+      <div className='opponent'></div>
+
+    </div>
   )
 }
